aa9907b237062dade232297aed2269730a988e29
nmiell@gmail.com <nmiell@gmail.com>
2008-03-14 22:23:19 -0700

    Fix the SSE related AMD64 build breakage
    The introduction of pixman_have_sse() copied the #ifdef __amd64__
    found around pixman_have_mmx() in pixman-mmx.c but didn't include the
    equivalents of the macros in pixman-mmx.h. As a result, the AMD64 link
    fails because the pixman_have_sse() function doesn't exist.
    
    I also introduced header inclusion guards to pixman-mmx.h and
    pixman-sse.h while I was at it.

--- pixman/pixman-sse.h
+++ pixman/pixman-sse.h
@@ -23,6 +23,9 @@
  * Author:  Rodrigo Kumpera (kumpera@gmail.com)
  * 
  */
+#ifndef _PIXMAN_SSE_H_
+#define _PIXMAN_SSE_H_
+
 #ifdef HAVE_DIX_CONFIG_H
 #include <dix-config.h>
 #endif
@@ -31,8 +34,20 @@
 
 #ifdef USE_SSE2
 
+#if !defined(__amd64__) && !defined(__x86_64__)
 pixman_bool_t pixman_have_sse(void);
+#else
+#define pixman_have_sse() TRUE
+#endif
+
+#else
+#define pixman_have_sse() FALSE
+#endif
+
+#ifdef USE_SSE2
 
 void fbComposeSetupSSE(void);
 
 #endif /* USE_SSE2 */
+
+#endif /* _PIXMAN_SSE_H_ */
