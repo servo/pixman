f2af00bf02dcf3e7e27ac3e035d41c387fc9400b
Ian Rickards <Ian.Rickards@arm.com>
2009-04-21 17:32:36 -0400

    Add support for ARM NEON fast paths
    Currently disabled

--- pixman/pixman-arm-detect-win32.asm
+++ pixman/pixman-arm-detect-win32.asm
@@ -1,9 +1,20 @@
     area pixman_msvc, code, readonly
 
-    export  pixman_msvc_try_armv6_op
+    export  pixman_msvc_try_arm_simd_op
 
-pixman_msvc_try_armv6_op
-    uqadd8 r0,r0,r1
+pixman_msvc_try_arm_simd_op
+    ;; I don't think the msvc arm asm knows how to do SIMD insns
+    ;; uqadd8 r3,r3,r3
+    dcd 0xe6633f93
+    mov pc,lr
+    endp
+
+    export  pixman_msvc_try_arm_neon_op
+
+pixman_msvc_try_arm_neon_op
+    ;; I don't think the msvc arm asm knows how to do NEON insns
+    ;; veor d0,d0,d0
+    dcd 0xf3000110
     mov pc,lr
     endp
 
