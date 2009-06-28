servo/pixman

commit 9d3f71d726c8b959b64c3e6b43ca4d3ccb320c32

Delete ARM header files

pixman/pixman-arm-neon.c
@@ -30,8 +30,6 @@
 #include <config.h>
 #endif
 
-#include "pixman-arm-neon.h"
-
 #include <arm_neon.h>
 #include <string.h>
 

pixman/pixman-arm-simd.c
@@ -27,8 +27,6 @@
 #include <config.h>
 #endif
 
-#include "pixman-arm-simd.h"
-
 void
 fbCompositeSrcAdd_8000x8000arm (
                             pixman_implementation_t * impl,

pixman/Makefile.am
@@ -90,8 +90,7 @@ endif
 if USE_ARM_SIMD
 noinst_LTLIBRARIES += libpixman-arm-simd.la
 libpixman_arm_simd_la_SOURCES = \
-	pixman-arm-simd.c \
-	pixman-arm-simd.h
+	pixman-arm-simd.c
 libpixman_arm_simd_la_CFLAGS = $(DEP_CFLAGS) $(ARM_SIMD_CFLAGS)
 libpixman_arm_simd_la_LIBADD = $(DEP_LIBS)
 libpixman_1_la_LIBADD += libpixman-arm-simd.la
@@ -101,8 +100,7 @@ endif
 if USE_ARM_NEON
 noinst_LTLIBRARIES += libpixman-arm-neon.la
 libpixman_arm_neon_la_SOURCES = \
-        pixman-arm-neon.c \
-        pixman-arm-neon.h
+        pixman-arm-neon.c
 libpixman_arm_neon_la_CFLAGS = $(DEP_CFLAGS) $(ARM_NEON_CFLAGS)
 libpixman_arm_neon_la_LIBADD = $(DEP_LIBS)
 libpixman_1_la_LIBADD += libpixman-arm-neon.la

pixman/pixman-arm-neon.h
@@ -1,179 +1,0 @@
-/*
- * Copyright © 2009 ARM Ltd
- *
- * Permission to use, copy, modify, distribute, and sell this software and its
- * documentation for any purpose is hereby granted without fee, provided that
- * the above copyright notice appear in all copies and that both that
- * copyright notice and this permission notice appear in supporting
- * documentation, and that the name of ARM Ltd not be used in
- * advertising or publicity pertaining to distribution of the software without
- * specific, written prior permission.  ARM Ltd makes no
- * representations about the suitability of this software for any purpose.  It
- * is provided "as is" without express or implied warranty.
- *
- * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS
- * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS, IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
- * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
- * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
- * SOFTWARE.
- *
- * Author:  Ian Rickards (ian.rickards@arm.com) 
- *
- */
-
-#include "pixman-private.h"
-
-#ifdef USE_ARM_NEON
-
-pixman_bool_t pixman_have_arm_neon(void);
-
-#else
-#define pixman_have_arm_neon() FALSE
-#endif
-
-#ifdef USE_ARM_NEON
-
-extern const pixman_fast_path_t *const arm_neon_fast_paths;
-
-void
-fbCompositeSrcAdd_8000x8000neon (
-                            pixman_implementation_t * impl,
-                            pixman_op_t op,
-                        pixman_image_t * pSrc,
-                        pixman_image_t * pMask,
-                        pixman_image_t * pDst,
-                        int32_t      xSrc,
-                        int32_t      ySrc,
-                        int32_t      xMask,
-                        int32_t      yMask,
-                        int32_t      xDst,
-                        int32_t      yDst,
-                        int32_t      width,
-                        int32_t      height);
-
-void
-fbCompositeSrc_8888x8888neon (
-                            pixman_implementation_t * impl,
-                            pixman_op_t op,
-			pixman_image_t * pSrc,
-			pixman_image_t * pMask,
-			pixman_image_t * pDst,
-			int32_t      xSrc,
-			int32_t      ySrc,
-			int32_t      xMask,
-			int32_t      yMask,
-			int32_t      xDst,
-			int32_t      yDst,
-			int32_t      width,
-			int32_t      height);
-
-void
-fbCompositeSrc_8888x8x8888neon (
-                            pixman_implementation_t * impl,
-                            pixman_op_t op,
-			pixman_image_t * pSrc,
-			pixman_image_t * pMask,
-			pixman_image_t * pDst,
-			int32_t      xSrc,
-			int32_t      ySrc,
-			int32_t      xMask,
-			int32_t      yMask,
-			int32_t      xDst,
-			int32_t      yDst,
-			int32_t      width,
-			int32_t      height);
-
-void
-fbCompositeSolidMask_nx8x0565neon (
-                            pixman_implementation_t * impl,
-                            pixman_op_t op,
-                        pixman_image_t * pSrc,
-                        pixman_image_t * pMask,
-                        pixman_image_t * pDst,
-                        int32_t      xSrc,
-                        int32_t      ySrc,
-                        int32_t      xMask,
-                        int32_t      yMask,
-                        int32_t      xDst,
-                        int32_t      yDst,
-                        int32_t      width,
-                        int32_t      height);
-
-void
-fbCompositeSolidMask_nx8x8888neon (
-                            pixman_implementation_t * impl,
-                            pixman_op_t op,
-			pixman_image_t * pSrc,
-			pixman_image_t * pMask,
-			pixman_image_t * pDst,
-			int32_t      xSrc,
-			int32_t      ySrc,
-			int32_t      xMask,
-			int32_t      yMask,
-			int32_t      xDst,
-			int32_t      yDst,
-		 	int32_t      width,
-			int32_t      height);
-
-void
-fbCompositeSrcAdd_8888x8x8neon (
-                            pixman_implementation_t * impl,
-                            pixman_op_t op,
-                        pixman_image_t * pSrc,
-                        pixman_image_t * pMask,
-                        pixman_image_t * pDst,
-                        int32_t      xSrc,
-                        int32_t      ySrc,
-                        int32_t      xMask,
-                        int32_t      yMask,
-                        int32_t      xDst,
-                        int32_t      yDst,
-                        int32_t      width,
-                        int32_t      height);
-
-void
-fbCompositeSrc_16x16neon (
-	pixman_implementation_t * impl,
-	pixman_op_t op,
-                        pixman_image_t * pSrc,
-                        pixman_image_t * pMask,
-                        pixman_image_t * pDst,
-	int32_t      xSrc,
-	int32_t      ySrc,
-	int32_t      xMask,
-	int32_t      yMask,
-	int32_t      xDst,
-	int32_t      yDst,
-	int32_t      width,
-	int32_t      height);
-
-void
-fbCompositeSrc_24x16neon (
-	pixman_implementation_t * impl,
-	pixman_op_t op,
-	pixman_image_t * pSrc,
-	pixman_image_t * pMask,
-	pixman_image_t * pDst,
-	int32_t      xSrc,
-	int32_t      ySrc,
-	int32_t      xMask,
-	int32_t      yMask,
-	int32_t      xDst,
-	int32_t      yDst,
-	int32_t      width,
-	int32_t      height);
-
-pixman_bool_t
-pixman_fill_neon (uint32_t *bits,
-		  int stride,
-		  int bpp,
-		  int x,
-		  int y,
-		  int width,
-		  int height,
-		  uint32_t _xor);
-
-#endif /* USE_ARM_NEON */

pixman/pixman-arm-simd.h
@@ -1,106 +1,0 @@
-/*
- * Copyright © 2008 Mozilla Corporation
- *
- * Permission to use, copy, modify, distribute, and sell this software and its
- * documentation for any purpose is hereby granted without fee, provided that
- * the above copyright notice appear in all copies and that both that
- * copyright notice and this permission notice appear in supporting
- * documentation, and that the name of Mozilla Corporation not be used in
- * advertising or publicity pertaining to distribution of the software without
- * specific, written prior permission.  Mozilla Corporation makes no
- * representations about the suitability of this software for any purpose.  It
- * is provided "as is" without express or implied warranty.
- *
- * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS
- * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS, IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
- * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
- * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
- * SOFTWARE.
- *
- * Author:  Jeff Muizelaar (jeff@infidigm.net)
- *
- */
-
-#include "pixman-private.h"
-
-#ifdef USE_ARM_SIMD
-
-pixman_bool_t pixman_have_arm_simd(void);
-
-#else
-#define pixman_have_arm_simd() FALSE
-#endif
-
-#ifdef USE_ARM_SIMD
-
-extern const pixman_fast_path_t *const arm_simd_fast_paths;
-
-void
-fbCompositeSrcAdd_8000x8000arm (
-                            pixman_implementation_t * impl,
-                            pixman_op_t op,
-				pixman_image_t * pSrc,
-				pixman_image_t * pMask,
-				pixman_image_t * pDst,
-				int32_t      xSrc,
-				int32_t      ySrc,
-				int32_t      xMask,
-				int32_t      yMask,
-				int32_t      xDst,
-				int32_t      yDst,
-				int32_t      width,
-				int32_t      height);
-
-void
-fbCompositeSrc_8888x8888arm (
-                            pixman_implementation_t * impl,
-                            pixman_op_t op,
-			 pixman_image_t * pSrc,
-			 pixman_image_t * pMask,
-			 pixman_image_t * pDst,
-				int32_t      xSrc,
-				int32_t      ySrc,
-				int32_t      xMask,
-				int32_t      yMask,
-				int32_t      xDst,
-				int32_t      yDst,
-				int32_t      width,
-				int32_t      height);
-
-void
-fbCompositeSrc_8888x8x8888arm (
-                            pixman_implementation_t * impl,
-                            pixman_op_t op,
-			 pixman_image_t * pSrc,
-			 pixman_image_t * pMask,
-			 pixman_image_t * pDst,
-				int32_t      xSrc,
-				int32_t      ySrc,
-				int32_t      xMask,
-				int32_t      yMask,
-				int32_t      xDst,
-				int32_t      yDst,
-				int32_t      width,
-				int32_t      height);
-
-void
-fbCompositeSolidMask_nx8x8888arm (
-                            pixman_implementation_t * impl,
-                            pixman_op_t op,
-			 pixman_image_t * pSrc,
-			 pixman_image_t * pMask,
-			 pixman_image_t * pDst,
-				int32_t      xSrc,
-				int32_t      ySrc,
-				int32_t      xMask,
-				int32_t      yMask,
-				int32_t      xDst,
-				int32_t      yDst,
-				int32_t      width,
-				int32_t      height);
-
-
-#endif /* USE_ARM */

commit 10aa32315529eaff848b8348cad47b2673f853cf

Really fix ARM build.
Commit 9d3f71d726c8b959b64c3e6b43ca4d3ccb320c32 broke the build
on ARM architectures by just removing custom include files and
not providing "pixman-private.h" as a replacement.

pixman/pixman-arm-neon.c
@@ -32,6 +32,7 @@
 
 #include <arm_neon.h>
 #include <string.h>
+#include "pixman-private.h"
 
 // Deal with an intrinsic that is defined differently in GCC
 #if !defined(__ARMCC_VERSION) && !defined(__pld)

pixman/pixman-arm-simd.c
@@ -27,6 +27,8 @@
 #include <config.h>
 #endif
 
+#include "pixman-private.h"
+
 static void
 fbCompositeSrcAdd_8000x8000arm (
                             pixman_implementation_t * impl,