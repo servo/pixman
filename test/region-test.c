967ff0bdc7f46806b7a6d16332ad39cf2c1f01c1
Søren Sandmann Pedersen <sandmann@redhat.com>
2009-07-07 22:55:32 -0400

    Add an initialization with an empty rectangle to region-test.c
    This should produce a valid region without empty rectangles in
    it. Currently it doesn't.

--- test/region-test.c
+++ test/region-test.c
@@ -3,21 +3,41 @@
 #include <stdio.h>
 #include "pixman.h"
 
-/* This used to go into an infinite loop before pixman-region.c
- * was fixed to not use explict "short" variables
- */
 int
 main ()
 {
     pixman_region32_t r1;
     pixman_region32_t r2;
     pixman_region32_t r3;
+    pixman_box32_t boxes[] = {
+	{ 10, 10, 20, 20 },
+	{ 30, 30, 30, 40 },
+    };
+    int i;
+    pixman_box32_t *b;
 
+    /* This used to go into an infinite loop before pixman-region.c
+     * was fixed to not use explict "short" variables
+     */
     pixman_region32_init_rect (&r1, 0, 0, 20, 64000);
     pixman_region32_init_rect (&r2, 0, 0, 20, 64000);
     pixman_region32_init_rect (&r3, 0, 0, 20, 64000);
 
     pixman_region32_subtract (&r1, &r2, &r3);
 
+
+    /* This would produce a region containing an empty
+     * rectangle in it. Such regions are considered malformed,
+     * but using an empty rectangle for initialization should
+     * work.
+     */
+    pixman_region32_init_rects (&r1, boxes, 2);
+
+    b = pixman_region32_rectangles (&r1, &i);
+    while (--i)
+    {
+	assert (b[i].x1 < b[i].x2);
+	assert (b[i].y1 < b[i].y2);
+    }
 }
 
