7dc3593d8172e292b39a7d6cc7772fcf9a04bb1f
Søren Sandmann Pedersen <sandmann@redhat.com>
2009-07-12 20:31:26 -0400

    Reformat and reindent pixman-solid-fill.c

--- pixman/pixman-solid-fill.c
+++ pixman/pixman-solid-fill.c
@@ -25,24 +25,29 @@
 #include "pixman-private.h"
 
 static void
-solid_fill_get_scanline_32 (pixman_image_t *image, int x, int y, int width, uint32_t *buffer,
-			    const uint32_t *mask, uint32_t mask_bits)
+solid_fill_get_scanline_32 (pixman_image_t *image,
+                            int             x,
+                            int             y,
+                            int             width,
+                            uint32_t *      buffer,
+                            const uint32_t *mask,
+                            uint32_t        mask_bits)
 {
     uint32_t *end = buffer + width;
     register uint32_t color = ((solid_fill_t *)image)->color;
-    
+
     while (buffer < end)
 	*(buffer++) = color;
-    
+
     return;
 }
 
 static source_pict_class_t
 solid_fill_classify (pixman_image_t *image,
-		     int	     x,
-		     int	     y,
-		     int	     width,
-		     int	     height)
+                     int             x,
+                     int             y,
+                     int             width,
+                     int             height)
 {
     return (image->source.class = SOURCE_IMAGE_CLASS_HORIZONTAL);
 }
@@ -58,17 +63,17 @@ static uint32_t
 color_to_uint32 (const pixman_color_t *color)
 {
     return
-	(color->alpha >> 8 << 24) |
-	(color->red >> 8 << 16) |
+        (color->alpha >> 8 << 24) |
+        (color->red >> 8 << 16) |
         (color->green & 0xff00) |
-	(color->blue >> 8);
+        (color->blue >> 8);
 }
 
 PIXMAN_EXPORT pixman_image_t *
 pixman_image_create_solid_fill (pixman_color_t *color)
 {
-    pixman_image_t *img = _pixman_image_allocate();
-    
+    pixman_image_t *img = _pixman_image_allocate ();
+
     if (!img)
 	return NULL;
 
@@ -80,6 +85,7 @@ pixman_image_create_solid_fill (pixman_color_t *color)
     img->common.property_changed = solid_fill_property_changed;
 
     solid_fill_property_changed (img);
-    
+
     return img;
 }
+
