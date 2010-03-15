de27f45ddd46fc48ec9598f2f177155328d55580
Søren Sandmann Pedersen <ssp@redhat.com>
2010-03-15 11:51:09 -0400

    Ensure that only the low 4 bit of 4 bit pixels are stored.
    In some cases we end up trying to use the STORE_4 macro with an 8 bit
    values, which resulted in other pixels getting overwritten. Fix this
    by always masking off the low 4 bits.
    
    This fixes blitters-test on big-endian machines.

--- test/blitters-test.c
+++ test/blitters-test.c
@@ -482,7 +482,7 @@ main (int argc, char *argv[])
 	    /* Predefined value for running with all the fastpath functions
 	       disabled. It needs to be updated every time when changes are
 	       introduced to this program or behavior of pixman changes! */
-	    if (crc == 0xEF7A1179)
+	    if (crc == 0xA058F792)
 	    {
 		printf ("blitters test passed\n");
 	    }
