a96efd02d68b726d6d140d0bd211bc7cc1be127a
Stefan Weil <sw@weilnetz.de>
2012-11-13 19:44:15 +0100

    Always use xmmintrin.h for 64 bit Windows
    MinGW-w64 uses the GNU compiler and does not define _MSC_VER.
    Nevertheless, it provides xmmintrin.h and must be handled
    here like the MS compiler. Otherwise compilation fails due to
    conflicting declarations.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- pixman/pixman-mmx.c
+++ pixman/pixman-mmx.c
@@ -62,7 +62,7 @@ _mm_empty (void)
 #endif
 
 #ifdef USE_X86_MMX
-# if (defined(__SUNPRO_C) || defined(_MSC_VER))
+# if (defined(__SUNPRO_C) || defined(_MSC_VER) || defined(_WIN64))
 #  include <xmmintrin.h>
 # else
 /* We have to compile with -msse to use xmmintrin.h, but that causes SSE
